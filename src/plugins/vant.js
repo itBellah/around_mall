35e10935ab947799495a12e1d7d5d6b368129cfe
pengzaier <837023886@qq.com>
2019-09-11 15:45:34 +0800

    完成了新闻列表

--- src/plugins/vant.js
+++ src/plugins/vant.js
@@ -1,8 +1,15 @@
 // 初始化 vant.js
 import Vue from 'vue'
 // 导入 Button 组件
-import { Button } from 'vant'
+import { Button, Card, Tag, PullRefresh, List, Cell, Loading, Toast} from 'vant'
 import 'vant/lib/index.css';
 
 // 挂载 Button 组件
 Vue.use(Button)
+  .use(Card)
+  .use(Tag)
+  .use(PullRefresh)
+  .use(List)
+  .use(Cell)
+  .use(Loading)
+  .use(Toast)
--- src/router.js
+++ src/router.js
@@ -2,16 +2,21 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './components/Home.vue'
 import Index from './components/Index.vue'
+import Newslist from './components/news/Newslist.vue'
+import Detail from './components/news/Detail.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     { path: '/', redirect: '/index' },
-    { path: '/home', redirect: '/index', component: Home, 
+    {
+      path: '/home', redirect: '/index', component: Home,
       children: [
         { path: '/index', component: Index }
       ]
-    }
+    },
+    { path: '/newslist', component: Newslist },
+    { path: '/newslist/detail', component: Detail}
   ]
 })
